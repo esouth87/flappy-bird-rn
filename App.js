68ecac0c8a246bbea1f1c5bb102deabb31f9b55b
esouth87 <edward.southall@uq.net.au>
2023-01-19 13:18:10 +1100

    Obstacles are currently appearing
    Finally got the obstacles to appear, rewrote App.js and Obstacles.js

--- App.js
+++ App.js
@@ -9,51 +9,17 @@ export default function App() {
 
   const birdLeft = screenWidth / 2
   const [birdBottom, setBirdBottom ] = useState(screenHeight/2)
+  const gravity = 3
+  let gameTimerId
+
   const [obstaclesLeft, setObstaclesLeft] = useState(screenWidth)
+  let obstaclesLeftTimerId
   const obstacleWidth = 60
   const obstacleHeight = 300
-  const gap = 20
-  const gravity = 3
-  const scrollSpeed = 5
-  let gameTimerId
-  let obstaclesTimerId
+  const gap = 200
 
   //console.log(screenHeight)
   //console.log(screenWidth)
-
-  const App = () => {
-    const [obstaclesLeft, setObstaclesLeft] = useState(screenWidth);
-    const [obstaclesRendered, setObstaclesRendered] = useState(false);
-    const obstacleWidth = 60;
-
-    useEffect(() => {
-      if (obstaclesLeft > -obstacleWidth) {
-        setObstaclesRendered(true);
-      } else {
-        setObstaclesRendered(false);
-      }
-    }, [obstaclesLeft]);
-
-    return (
-      <View>
-        <Obstacles obstaclesLeft={obstaclesLeft} obstacleWidth={obstacleWidth} />
-        <RenderChecker obstaclesRendered={obstaclesRendered} />
-      </View>
-    );
-
-
-  const RenderChecker = ({ obstaclesRendered }) => {
-    return obstaclesRendered ? (
-      <View>
-        <Text>Obstacles are rendering</Text>
-      </View>
-    ) : (
-      <View>
-        <Text>Obstacles are not rendering</Text>
-      </View>
-    );
-  };
-
 
   //start bird falling
   useEffect(() => {
@@ -67,40 +33,39 @@ export default function App() {
       }
     }
   }, [birdBottom])
-//  console.log(birdBottom)
+  console.log(birdBottom)
 
   //start first obstacles
-
   useEffect(() => {
-    if (obstaclesLeft < screenWidth) {
-      obstaclesTimerId = setInterval(() => {
-        setObstaclesLeft(obstaclesLeft => obstaclesLeft - scrollSpeed)
+    if (obstaclesLeft > -obstacleWidth) {
+      obstaclesLeftTimerId = setInterval(() => {
+        setObstaclesLeft(obstaclesLeft => obstaclesLeft - 5)
       }, 30)
+
+      return () => {
+        clearInterval(obstaclesLeftTimerId)
+      }
+
     }
-    return () => {
-      clearInterval(obstaclesTimerId)
-    }
+
+
+
   }, [obstaclesLeft])
 
-
-}
-
   return (
-    <>
+    <View style={styles.container}>
         <Bird
           birdBottom={birdBottom}
           birdLeft={birdLeft}
         />
 
       <Obstacles
-        obstacleWidth={obstacleWidth}
+        obstacleWidth = {obstacleWidth}
         obstacleHeight={obstacleHeight}
-        gap={gap}
+        gap = {gap}
         obstaclesLeft={obstaclesLeft}
       />
-
-      <RenderChecker obstaclesRendered={obstaclesRendered} />
-    </>
+    </View>
   );
 }
 
@@ -108,7 +73,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-//    alignItems: 'center',
-//    justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
--- components/Obstacles.js
+++ components/Obstacles.js
@@ -2,31 +2,27 @@ import React from 'react'
 import { View } from 'react-native'
 
 const Obstacles = ({obstaclesLeft, obstacleWidth, obstacleHeight, gap}) => {
-  //if (obstaclesLeft > 0 -obstacleWidth) {
+
   return (
-    <View>
+    <>
       <View style={{
-          position: 'absolute',
-          backgroundColor: 'blue',
-          width: obstacleWidth,
-          height: obstacleHeight,
-          left: obstaclesLeft,
-          bottom: 10 + obstacleHeight + gap,
+        position: 'absolute',
+        backgroundColor: 'green',
+        width: obstacleWidth,
+        height: obstacleHeight,
+        left: obstaclesLeft,
+        bottom: 0 + obstacleHeight + gap,
+      }}/>
+      <View style={{
+        position: 'absolute',
+        backgroundColor: 'green',
+        width: obstacleWidth,
+        height: obstacleHeight,
+        left: obstaclesLeft,
+        bottom: 0,
       }}
       />
-      <View style={{
-          position: 'absolute',
-          backgroundColor: 'green',
-          width: obstacleWidth,
-          height: obstacleHeight,
-          left: obstaclesLeft,
-          bottom: 0,
-      }}/>
-    </View>
-  )
-//} //else {
-//  return null;
-//}
+    </>  )
 }
 
 export default Obstacles
